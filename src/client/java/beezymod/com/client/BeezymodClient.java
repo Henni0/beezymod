adab2b86f0d3ffba40ff293625fb75fcc72a8a8e
Jan <jan@agent-ally.de>
2026-05-02 22:07:44 +0200

    initial commit

---
+++ src/client/java/beezymod/com/client/BeezymodClient.java
@@ -0,0 +1,10 @@
+package beezymod.com.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeezymodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
